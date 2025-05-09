ae9f3c622360b7dba149547063803ae8632b0af0
Adler Castro <adlercastro01@gmail.com>
2025-05-09 18:30:30 -0300

    :wrench: chore: Tipagem da env com o Zod

---
+++ src/config/env.config.ts
@@ -0,0 +1,8 @@
+import z from 'zod';
+
+const envSchema = z.object({
+  NEXT_PUBLIC_ENV: z.enum(['development', 'production', 'test']),
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+});
+
+export const env = envSchema.parse(process.env);
