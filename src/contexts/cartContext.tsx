a445212bea0e75597f9a6cfe6482ae42b33689af
Adler Castro <adlercastro01@gmail.com>
2025-05-09 18:09:12 -0300

    :sparkles: feat: Contexto do carrinho de compras junto com suas funções

---
+++ src/contexts/cartContext.tsx
@@ -0,0 +1,76 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+import { CartItem } from '@/types/cart.type';
+
+interface CartContextType {
+  cartItems: CartItem[];
+  addToCart: (item: number) => void;
+  updateQuantity: (id: number, quantity: number) => void;
+  removeFromCart: (id: number) => void;
+  clearCart: () => void;
+}
+
+const CartContext = createContext({} as CartContextType);
+
+export function CartProvider({ children }: { children: ReactNode }) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  function addToCart(productId: number) {
+    setCartItems((state) => {
+      const productInCart = state.some((item) => item.id === productId);
+
+      if (productInCart) {
+        return state.map((item) => {
+          if (item.id === productId) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          } else {
+            return item;
+          }
+        });
+      } else {
+        return [...state, { id: productId, quantity: 1 }];
+      }
+    });
+  }
+
+  function updateQuantity(id: number, quantity: number) {
+    setCartItems((state) =>
+      state.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            quantity,
+          };
+        } else {
+          return item;
+        }
+      }),
+    );
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems((state) => state.filter((item) => item.id !== id));
+  }
+
+  function clearCart() {
+    setCartItems([]);
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addToCart,
+        updateQuantity,
+        removeFromCart,
+        clearCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export const useCart = () => useContext(CartContext);
