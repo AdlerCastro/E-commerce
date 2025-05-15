4091cec3dde1db73207b410a9d11321ff153bc37
Adler Castro <adlercastro01@gmail.com>
2025-05-15 08:36:29 -0300

    :bug: fix: A ausência de parâmetros estáticos causa o erro de build

--- src/app/(home)/products/[slug]/page.tsx
+++ src/app/(home)/products/[slug]/page.tsx
@@ -1,5 +1,12 @@
 import { getProduct } from '@/actions/getProduct';
+import { getProducts } from '@/actions/getProducts';
 import ProductPage from '@/components/templates/product';
+
+export async function generateStaticParams() {
+  const response = await getProducts();
+
+  return response.products.map((product) => ({ slug: product.slug }));
+}
 
 export default async function Product({
   params,
