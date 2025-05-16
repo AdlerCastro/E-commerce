4997f54b66dfbd4c96d9bbf4fc3220ea63ad87fc
Adler Castro <adlercastro01@gmail.com>
2025-05-16 10:43:20 -0300

    :sparkles: feat: Página de checkout do pedido

---
+++ src/app/(home)/checkout/page.tsx
@@ -0,0 +1,9 @@
+import CheckoutPage from '@/components/templates/checkout';
+
+export default function Checkout() {
+  return (
+    <div className='flex w-full justify-center'>
+      <CheckoutPage />
+    </div>
+  );
+}
